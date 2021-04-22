7b7e0f8e64a23bc69a7bf7a340fb04c950448c66
Daniel <danieldiazgonza52@gmail.com>
2021-04-22 18:10:25 -0500

    Added explanation

---
+++ explanation/book_class.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self, title, color):
+        self.title = title
+        self.color = color
+
+# Instance objects of Book class
+blue_book = Book("The blue kid", "Blue")
+green_book = Book("The frog story", "Green")
